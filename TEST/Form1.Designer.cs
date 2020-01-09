c07f67768a4c4ba1014072a0218e9ca4129dd057
windking2256@gmail.com <windking2256@gmail.com>
2020-01-09 19:54:50 +0800

    加入專案檔案。

---
+++ TEST/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace TEST
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtinput = new System.Windows.Forms.TextBox();
+            this.txtoutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(177, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "轉換";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtinput
+            // 
+            this.txtinput.Location = new System.Drawing.Point(37, 64);
+            this.txtinput.Name = "txtinput";
+            this.txtinput.Size = new System.Drawing.Size(100, 22);
+            this.txtinput.TabIndex = 1;
+            // 
+            // txtoutput
+            // 
+            this.txtoutput.Location = new System.Drawing.Point(306, 64);
+            this.txtoutput.Name = "txtoutput";
+            this.txtoutput.Size = new System.Drawing.Size(100, 22);
+            this.txtoutput.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 146);
+            this.Controls.Add(this.txtoutput);
+            this.Controls.Add(this.txtinput);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtinput;
+        private System.Windows.Forms.TextBox txtoutput;
+    }
+}
+
---
+++ TEST/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TEST
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int hour = 0;
+        string c = ":";        
+        bool Colon()
+        {
+            String Time = txtinput.Text;
+            int startIndex = 2;
+            int length = 1;
+            String colon = Time.Substring(startIndex, length);
+            bool result = colon.Equals(c);
+            return result;
+        }
+        int MIN(int hour)
+        {
+            
+            String Time = txtinput.Text;
+            int startIndex = 3;
+            int length = 2;
+            String Mintime = Time.Substring(startIndex, length);
+            if (int.TryParse(Mintime, out int min))
+            {
+                if (min >= 0 && min <= 59)
+                {
+                    if (hour <= 11 &&  hour > 0)
+                    {
+                        txtoutput.Text = txtoutput.Text + ":" + Mintime + " AM";
+                        return hour;
+                    }
+                    else if(hour == 0)
+                    {
+                        txtoutput.Text = hour+12 + ":" + Mintime + " AM";
+                    }
+                    else if(hour >= 13 && hour <=21)
+                    {
+                        txtoutput.Text = "0"+ txtoutput.Text + ":" + Mintime + " PM";
+                        return hour;
+                    }
+                    else
+                    {
+                        txtoutput.Text = txtoutput.Text + ":" + Mintime + " PM";
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("ERROR");
+                    Application.Exit();
+                }
+            }
+            else
+            {
+                MessageBox.Show("ERROR");
+                Application.Exit();
+            }
+            return 0;
+        }
+        int HR()
+        {
+            int G;
+            String Time = txtinput.Text;
+            int startIndex = 0;
+            int length = 2;
+            String Hourtime = Time.Substring(startIndex, length);
+            if (int.TryParse(Hourtime, out int hour))
+            {
+                if (hour <= 12)
+                {
+                    txtoutput.Text = Hourtime;
+                    return hour;
+                }
+                else if (hour > 12 && hour < 24)
+                {
+                    G = hour - 12;
+                    txtoutput.Text = G.ToString();
+                    return hour;
+                }
+                else
+                {
+                    MessageBox.Show("ERROR");                   
+                    Application.Exit();
+                }
+            }
+            else
+            {
+                MessageBox.Show("ERROR");
+                Application.Exit();
+            }
+            return hour;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtinput.TextLength != 5)
+                {
+                    MessageBox.Show("ERROR");
+                    Application.Exit();
+                    return;
+                }
+                if (Colon() != true)
+                {
+                    MessageBox.Show("ERROR");
+                    Application.Exit();
+                }
+                hour = HR();
+                MIN(hour);
+            }
+            catch(Exception exp)
+            {
+                Console.WriteLine(exp.ToString());
+                throw ;
+            }                             
+        }
+    }
+}
